4207a3bad251221d5d80cd74b1218033e8c14818
Koen Hilgersom <khilgersom@users.noreply.github.com>
2016-09-27 10:02:34 +0200

    Update funLoadUltimaXML.m

--- funLoadUltimaXML.m
+++ funLoadUltimaXML.m
@@ -38,7 +38,7 @@ for z=(channels(:))'
     nMin  = 1; nMax = 1e9;
     PT100 = NaN(len,2);
     time  = NaN(len,1);
-    dist  = [];
+    Dist  = [];
     Data  = NaN(cntMax,0,3);
     %
     h2 = waitbar(0,['Progress on current channel (Channel ' num2str(z) ')'],'Position',get(h1,'Position')-[0 80 0 0]);
@@ -51,28 +51,28 @@ for z=(channels(:))'
             nodeNrP1= testNodeNumbersNaN(strListP1,nodeNrP1,XRead);      nodeNrP2= testNodeNumbersNaN(strListP2,nodeNrP2,XRead);
             tstNaN  = true;
         end                       
-        [nodeNrD] = testNodeNumbersChange(strListD, nodeNrD, XRead); % check for changed node numbers for dist/T/Sto/aSto data
+        [nodeNrD] = testNodeNumbersChange(strListD, nodeNrD, XRead); % check for changed node numbers for Dist/T/Sto/aSto data
         child           = XRead.getChildNodes.item(nodeNrD(1)).item(nodeNrD(2)).item(nodeNrD(3));
         childNodes      = child.getChildNodes;
         numChildNodes   = (childNodes.getLength-nodeNrD(4))/2;
         nEnd            = min(nMax,numChildNodes);
-        for n=nMin:min([length(dist),nEnd])
+        for n=nMin:min([length(Dist),nEnd])
             Read                 = sscanf(char(child.item(2*(n-1)+nodeNrD(4)).item(0).getData),'%f,%f,%f,%f');
             Data(cnt,n-nMin+1,:) = Read(2:4);    
         end        
-        if length(dist)<nEnd
-            nSt     = length(dist)+1;
+        if length(Dist)<nEnd
+            nSt     = length(Dist)+1;
             DataTmp = NaN(nEnd-size(Data,2),3);
-            distAdd = NaN(nEnd-length(dist),1);
+            distAdd = NaN(nEnd-length(Dist),1);
             for n=1:(nEnd-nSt+1)
                 Read         = sscanf(char(child.item(2*(n-1)+nodeNrD(4)).item(0).getData),'%f,%f,%f,%f');
                 if Read(1)>xR(2); nMax = nSt + n - 2; nEnd = nMax; break; end                
                 distAdd(n)   = Read(1);
                 DataTmp(n,:) = Read(2:4);
             end
-            dist = [dist; distAdd(~isnan(distAdd))];
-            if k==1;    nMin = max([nMin,find(dist<=xR(1),1,'last' )+1]); end
-                        nMax = min([nMax,find(dist>=xR(2),1,'first')-1]);
+            Dist = [Dist; distAdd(~isnan(distAdd))];
+            if k==1;    nMin = max([nMin,find(Dist<=xR(1),1,'last' )+1]); end
+                        nMax = min([nMax,find(Dist>=xR(2),1,'first')-1]);
             Data = cat(2,Data,NaN(cntMax,(nEnd-nMin+1)-size(Data,2),3));
             Data(cnt,(max(nSt,nMin):min(nEnd,nMax))-max(nSt,nMin)+nSt,:) = DataTmp(max(nSt,nMin):min(nEnd,nMax)-nSt+1,:);
             clear DataTmp distAdd
@@ -105,11 +105,11 @@ for z=(channels(:))'
     else
         time = time + timeCorr;
     end
-    dist = dist(nMin:min([nMax,length(dist)]));
+    Dist = Dist(nMin:min([nMax,length(Dist)]));
     %
     for f = 1 : length(tmpFN); eval([char(tmpFN(f)) '=dlmread(''' char(tmpFN(f)) '.tmpMF'');']); end
     for f = 1 : length(tmpFN); delete([char(tmpFN(f)) '.tmpMF']); end
-    save([mainDir filesep 'channel ' num2str(z) '.mat'],'Temp','Sto','ASto','time','dist','PT100')
+    save([mainDir filesep 'channel ' num2str(z) '.mat'],'Temp','Sto','ASto','time','Dist','PT100')
     disp(' '); disp(['Data for Channel ' num2str(z) ' was stored to:']); disp([mainDir filesep 'channel ' num2str(z) '.mat']); disp(' ');
     %
     waitbar((chCnt+0.99*k/len)/lenCh,h1);
